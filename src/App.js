632dd52571cd27a906f5642062161553cbb6024a
Fuentes Cristian <110264073+Fuentes-Cristian@users.noreply.github.com>
2022-12-16 14:26:33 -0300

    ajustes realizados

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './componentes/Navbar/Navbar';
 import Card from './componentes/card/Card';
 import Footer from './componentes/footer/Footer';
 import Login from './componentes/Login/Login.jsx';
-import Errror404 from './componentes/404/Errror404';
+import Error404 from './componentes/404/Error404';
 import Carrito from './componentes/Carrito/Carrito';
 import { BrowserRouter as Router,Routes,  Route, NavLink } from 'react-router-dom'
 
@@ -89,6 +89,8 @@ function App() {
                     <Routes>
                           <Route path='/' element = {<Login/>} />
                           <Route path='/Carrito' element = {<Carrito/>} />
+                          <Route path='/Error404' element = {<Error404/>} />
+
                     </Routes>
         </Router>
       <div>
--- src/componentes/404/Errror404.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Errror404 = () => {
-  return (
-    <h1>Errror404</h1>
-  )
-}
-
-export default Errror404
