1a59c248b7c91081888dcfd9e7f296fcaf3aaa81
Fuentes Cristian <110264073+Fuentes-Cristian@users.noreply.github.com>
2022-12-28 14:54:42 -0300

    codigo copiado para verifcar y sacar dudas

---
+++ src/componentes/contador/Contador.jsx
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react'
+import { useState } from 'react';
+import { CarritoContext } from '../context/CarritoContext';
+
+const contador = (props) => {
+
+    const { initialValue , item } = props;
+    const {guardarCarrito} = useContext(CarritoContext)
+
+    const [Contador, setContador] = useState(initialValue);
+    const {stock} =  item
+
+    const sumar = () => {
+
+        if(Contador < stock) {
+            setContador(contador + 1)
+        }
+    }
+
+    const restar = () => {
+
+        if( contador > 1) {
+            setContador(contador - 1)
+        }
+    }
+
+    const producto = {
+      ...item,
+      cantidad:count
+    }
+
+
+
+  return (
+    <div>
+        <button onClick={sumar}>Sumar</button>
+        <h1>{count}</h1>
+        <button onClick={restar}>Restar</button>
+        <br/>
+        <button onClick={() => guardarCarrito(producto)}>Agregar al carrito</button>
+    </div>
+  )
+}
+
+export default ItemCount
--- src/context/carritoContext.jsx
+++ src/context/carritoContext.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React,{createContext, useState} from 'react'
 
-export const CarritoContext = () => {
+
+export const CarritoContext = createContext('');
+
+
+const CarritoContextProvider = ({children}) => {
+    const productos = [
+       
+      ]
+
+      const [carrito , setCarrito] = useState([]);
+
+      const guardarCarrito = (producto) => {
+        setCarrito([...carrito,producto])
+      }
+
+
   return (
-    <h1>CarritoContext</h1>
+    <CarritoContext.Provider value={{productos,carrito,guardarCarrito}}>
+       {children}
+    </CarritoContext.Provider>
   )
 }
+
+export default CarritoContextProvider
