8446cdbd23e46d41155bb607eee1b2e75ee42252
Fuentes Cristian <110264073+Fuentes-Cristian@users.noreply.github.com>
2022-12-29 15:06:44 -0300

    cambios guardados

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -10,6 +10,50 @@ const ItemListContainer = () => {
       id: 1,
       nombre: "kassie"
     },
+    {
+      id: 2,
+      titulo: "Stefanie",
+      precio: "$2.500"
+    },
+    {
+      id: 3,
+      titulo: "Josef",
+      precio: "$2.500"
+    },
+    {
+      id: 4,
+      titulo: "Bella",
+      precio: "$2.500"
+    },
+    {
+      iid: 5,
+      titulo: "Rosie",
+      precio: "$2.500"
+    },
+    {
+      id: 6,
+      titulo: "Steff",
+      precio: "$2.500"
+    },
+    {
+      id: 7,
+      titulo: "Lorem",
+      precio: "$2.500"
+    },
+    {
+      id: 8,
+      titulo: "Cloe",
+      precio: "$2.500"
+    },
+    {id: 9,
+      titulo: "Juliet",
+      precio: "$2.500"
+    },
+    {
+      idid: 10,
+      titulo: "Roma",
+      precio: "$2.500"
+    },
   ]
 
 
